9c8f755ae367b3acfd814162ecf8d039c0dccf9c
Pratik Chavan <chavanpratik.pc@gmail.com>
2023-02-04 19:53:03 -0500

    deletion

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Timer } from "./Timer";
-export { default as CurrentTasks } from "./Tasks/CurrentTasks";
-export { default as AddTasks } from "./Tasks/AddTask";
-export { default as Music } from "./Music";
-export { default as Reports } from "./Reports";
-
-// export { default as AddTasks } from "./AddTask";
--- src/utils/DisplayTime.js
+++
@@ -1,5 +0,0 @@
-export const dispSecondsAsMins = (seconds) => {
-  const mins = Math.floor(seconds / 60);
-  const seconds_ = seconds % 60;
-  return mins.toString() + ":" + (seconds_ == 0 ? "00" : seconds_.toString());
-};
