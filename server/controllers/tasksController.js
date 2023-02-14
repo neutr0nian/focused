383a467f26f400495a5c9e8f2c0871c04cf881ca
Pratik Chavan <pratikc@youronlinemarketplace.com>
2023-02-14 00:32:50 -0500

    added task association with user

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,4 +1,5 @@
 const Tasks = require("../models/tasks");
+const User = require("../models/users");
 const Status = require("http-status-codes").StatusCodes;
 const { getRequestBody } = require("../utils/request.js");
 
@@ -9,6 +10,20 @@ module.exports = {
 
     Tasks.create(newTask)
       .then((task) => {
+        //associate the new task to the user
+        User.findByIdAndUpdate(res.locals.user._id, {
+          $push: { tasks: task._id },
+        })
+          .then((user) => console.log("Added task to user account", user))
+          .catch((error) => {
+            console.error(
+              `Error while adding task to the user account: ${error.message}`
+            );
+            res.status(Status.INTERNAL_SERVER_ERROR);
+            res.send({
+              message: "Failed: Adding task to the user account",
+            });
+          });
         res.status(Status.OK);
         res.send({
           message: `Task added successfully ${newTask.title}`,
@@ -26,10 +41,11 @@ module.exports = {
       res.send({
         message: "User not logged in",
       });
+      return;
     }
 
-    let userId = res.locals.user._id;
-    Tasks.find({ userId: userId })
+    let user = res.locals.user;
+    Tasks.find({ _id: { $in: user.tasks } })
       .then((task) => {
         console.log(`Task fetched successfully ${task}`);
         res.status(Status.OK);
--- server/models/users.js
+++ server/models/users.js
@@ -18,11 +18,8 @@ const userSchema = mongoose.Schema({
     lowercase: true,
     unique: true,
   },
-  // password: {
-  //   type: String,
-  //   required: true,
-  // },
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }],
 });
 
 userSchema.plugin(passportLocalMongoose, {
